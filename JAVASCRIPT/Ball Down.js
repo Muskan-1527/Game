d307f9ea10374b9f3017ca1da8fecaf1ea1cf347
muskan-1527 <agrawalm8832gmail.com>
2019-08-24 13:23:12 +0530

    created canvas

--- JAVASCRIPT/Ball Down.js
+++ JAVASCRIPT/Ball Down.js
@@ -1,21 +1,13 @@
-function ballMove()
-{
-	var elem=document.getElementById("ball");
-    var pos=0;
-    var id=setInterval(frame,5);
-    function frame()
-    {
-    	if(pos==600)
-    	{
-    		clearInterval(id);
-    	}
-
-    	else
-    	{
-    		pos++;
-    		elem.style.top=pos+"px";
-    	}
-    }
+function startGame() {
+	myGameArea.start();
 }
 
-
+var myGameArea = {
+	canvas : document.createElement("canvas") ,
+	start : function() {
+		this.canvas.width = 960;
+		this.canvas.height = 540;
+		this.context = this.canvas.getContext("2d");
+		document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+	}
+}
